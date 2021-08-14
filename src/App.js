1d94143cb98d02495601653206f68a1816bf44e2
Mehmet Seven <mehmetseven0@gmail.com>
2021-08-14 14:11:26 +0300

    mqtt service

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { useEffect } from 'react';
 import './App.css';
 
 import mqttService from './mqtt-service';
-const client = mqttService.getClient(console.log);
 
 function App() {
   useEffect(() => {
+    const client = mqttService.getClient(console.log);
+
     mqttService.onMessage(client, (d) => {
       console.log(d);
     });
 
     mqttService.subscribe(client, 'topic1');
+
+    return () => mqttService.closeConnection(client);
   }, []);
 
   const publish = () => {};
