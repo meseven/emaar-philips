9150ffe67839f22ee21f1b58fac5be50b0f335a0
Mehmet Seven <mehmetseven0@gmail.com>
2021-08-15 14:52:21 +0300

    thermostat modal

--- src/components/ThermostatModal.js
+++ src/components/ThermostatModal.js
@@ -2,7 +2,9 @@ import { useState, useEffect, memo } from 'react';
 
 import temprature from '../assets/temprature.png';
 import power_on from '../assets/power_on.png';
-// import power_off from '../assets/power_off.png';
+import power_off from '../assets/power_off.png';
+import cooling from '../assets/cooling.png';
+import heating from '../assets/heating.png';
 
 import { subscribe, unsubscribe, onMessage } from '../mqtt-service';
 
@@ -33,25 +35,40 @@ function ThermostatModal({ isModalVisible, closeModal, thermostat_id }) {
     ? serviceData[`FCU_${thermostat_id}_ROOMT_R`] / 50
     : null;
 
+  const powerStatus = serviceData.hasOwnProperty(`FCU_${thermostat_id}_ON_R`)
+    ? serviceData[`FCU_${thermostat_id}_ON_R`]
+    : null;
+
+  const coolingStatusKey = `FCU_${thermostat_id}_MODE_R`;
+  const coolingStatus = serviceData.hasOwnProperty(coolingStatusKey)
+    ? serviceData[coolingStatusKey]
+    : null;
+
   return (
     <Modal
       title={thermostat && thermostat.title}
       visible={isModalVisible}
-      width={'80%'}
+      width={'40%'}
       footer={null}
       onCancel={closeModal}
     >
       {/* <div>{JSON.stringify(serviceData, null, 2)}</div> */}
 
       <div className="modal-head">
-        <div className="left">left</div>
+        <div className="left">
+          <img
+            src={coolingStatus === 1 ? heating : cooling}
+            alt=""
+            className="cooling_status_img"
+          />
+        </div>
         <div className="center">
           <img src={temprature} width={40} alt="" />
           {roomTemprature && <h1>{roomTemprature} °C</h1>}
         </div>
         <div className="right">
           <a href="#/">
-            <img src={power_on} alt="" className="power_btn" />
+            <img src={powerStatus === 1 ? power_on : power_off} alt="" className="power_btn" />
           </a>
         </div>
       </div>
