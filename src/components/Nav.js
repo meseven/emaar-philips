f887dcb6db1049fc655df3005afbaead66d52e35
Mehmet Seven <mehmetseven0@gmail.com>
2021-11-15 17:55:59 +0300

    tooltip

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,41 @@
-import {NavLink} from "react-router-dom";
-import {AiFillHome} from "react-icons/ai"
-import {BiWater} from "react-icons/bi"
-import {FaServer, FaCog} from "react-icons/fa"
-import {GrSchedule} from "react-icons/gr"
-import {GiHeatHaze} from "react-icons/gi"
+import { memo } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/ai';
+import { BiWater } from 'react-icons/bi';
+import { FaServer, FaCog } from 'react-icons/fa';
+import { GrSchedule } from 'react-icons/gr';
+import { Tooltip } from 'antd';
 
 function Nav() {
-    return (
-        <nav className="menu">
-            <NavLink activeClassName="active" exact to="/">
-                <AiFillHome size={20}/>
-            </NavLink>
-            <NavLink activeClassName="active" to="/trench-heaters">
-                <GiHeatHaze size={20}/>
-            </NavLink>
-            <NavLink activeClassName="active" to="/water-leakages">
-                <BiWater size={20}/>
-            </NavLink>
-            <NavLink activeClassName="active" to="/server-rooms">
-                <FaServer size={20} />
-            </NavLink>
-            <NavLink activeClassName="active" to="/weekly-program">
-                <GrSchedule size={20} />
-            </NavLink>
-            <NavLink activeClassName="active" to="/settings">
-                <FaCog size={20} />
-            </NavLink>
-        </nav>
-    )
+  return (
+    <nav className="menu">
+      <NavLink activeClassName="active" exact to="/">
+        <Tooltip placement="right" title={'Thermostats'} color="#50d6e5">
+          <AiFillHome size={20} />
+        </Tooltip>
+      </NavLink>
+      <NavLink activeClassName="active" to="/water-leakages">
+        <Tooltip placement="right" title={'Water Leakages'} color="#50d6e5">
+          <BiWater size={20} />
+        </Tooltip>
+      </NavLink>
+      <NavLink activeClassName="active" to="/server-rooms">
+        <Tooltip placement="right" title={'Server Rooms'} color="#50d6e5">
+          <FaServer size={20} />
+        </Tooltip>
+      </NavLink>
+      <NavLink activeClassName="active" to="/weekly-program">
+        <Tooltip placement="right" title={'Weekly Program'} color="#50d6e5">
+          <GrSchedule size={20} />
+        </Tooltip>
+      </NavLink>
+      <NavLink activeClassName="active" to="/settings">
+        <Tooltip placement="right" title={'Settings'} color="#50d6e5">
+          <FaCog size={20} />
+        </Tooltip>
+      </NavLink>
+    </nav>
+  );
 }
 
-export default Nav
+export default memo(Nav);
